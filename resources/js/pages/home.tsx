64fb8dfe8afb39548c33cc281d99c4954b9a6778
BoneOg <vaughnpereyra098@gmail.com>
2025-06-09 15:50:09 +0800

    working simple

---
+++ resources/js/pages/home.tsx
@@ -0,0 +1,12 @@
+import { Head } from '@inertiajs/react';
+
+export default function Home() {
+  return (
+    <>
+      <Head title="Home" />
+      <div className="p-6 text-center text-xl font-semibold">
+        Hello, world!
+      </div>
+    </>
+  );
+}
